de39c229b4915b24a5291b5275f27f9c51caf402
Sarathkrishnan Ramesh <sarathkrishnan99@gmail.com>
2019-03-06 16:18:23 +0530

    Add visual marker for waypoints in Rviz - Add visual markers for each waypoint that is fetched from the list of waypoints - Add odom and gps topic parameters in the launch file - Fixed out of bounds error

--- scripts/waypoint_server.py
+++ scripts/waypoint_server.py
@@ -6,6 +6,7 @@ from __future__ import print_function
 from math import radians, sin, cos, sqrt, atan2, pi
 from std_msgs.msg import Float32
 from nav_msgs.msg import Odometry
+from visualization_msgs.msg import Marker
 import rospkg
 import rospy
 import json
@@ -59,7 +60,8 @@ class WaypointServer(object):
         self.origin_pos_y = 0.0
         self.origin_pos_z = 0.0
 
-        self.wp_num = 0
+        self.wp_num = -1
+        self.num_of_waypoints = 0.0
         self.dist_from_origin = 0.0
         self.disp_to_wp = 0.0
 
@@ -67,7 +69,6 @@ class WaypointServer(object):
             "/waypoint_server/threshold_distance")
 
         self.debug = True
-        # self.reset_all = True
 
         self.wp_update_state = False
         self.new_waypoint = False
@@ -78,38 +79,40 @@ class WaypointServer(object):
         self.gps_validity_timeout = 10.0
         self.last_valid_fix_time = rospy.get_rostime()
 
+        self.gps_topic = rospy.get_param(
+            "/waypoint_server/gps_topic")
+        self.odom_topic = rospy.get_param(
+            "/waypoint_server/odom_topic")
+
         self.nav_goal_pub = rospy.Publisher(
             'move_base_simple/goal', PoseStamped, queue_size=10)
         self.dist_pub = rospy.Publisher(
             'displacementToWaypoint', Float32, queue_size=10)
+        self.marker_pub = rospy.Publisher(
+            '/waypoint_marker', Marker, queue_size=10)
 
         self.pkg = rospkg.RosPack().get_path('waypoint_server')
 
     def run_server(self):
         rate = rospy.Rate(0.5)
-        rospy.Subscriber("/gps", NavSatFix, self.gps_subscriber)
-        rospy.Subscriber("/odom", Odometry, self.robot_pose_subscriber)
+        rospy.Subscriber(self.gps_topic, NavSatFix, self.gps_subscriber)
+        rospy.Subscriber(self.odom_topic, Odometry, self.robot_pose_subscriber)
         while not rospy.is_shutdown():
             rate.sleep()
             if self.gps_fix:
                 self.waypoint_publisher()
 
     def waypoint_publisher(self):
-        self.fetch_next_waypoint()
-        # if self.gps_fix_valid and self.new_waypoint:
-        if self.new_waypoint:
-            self.pose_publisher()
-            self.new_waypoint = False
+        if not self.wp_update_state or self.wp_num < self.num_of_waypoints and self.disp_to_wp <= self.threshold_distance:
+            self.fetch_next_waypoint()
 
-    # def gps_fix_valid(self):
-    #     time = rospy.get_time()
+            if self.new_waypoint:
+                self.pose_publisher()
+                self.new_waypoint = False
 
-    #     if (time - self.last_valid_fix_time.secs) < self.gps_validity_timeout:
-    #         return True
-
-    #     rospy.loginfo("GPS Fix Invalid! Last valid update was: %f seconds ago",
-    #                   time - self.last_valid_fix_time.secs)
-    #     return False
+        elif self.wp_num==self.num_of_waypoints and self.disp_to_wp <= 2:
+            rospy.loginfo("All waypoint covered successfully!")
+            rospy.loginfo("Robot has reached the destination.")
 
     def pose_publisher(self):
         desired_pose = PoseStamped()
@@ -133,6 +136,7 @@ class WaypointServer(object):
         desired_pose.pose.orientation.w = 1
 
         self.nav_goal_pub.publish(desired_pose)
+        self.marker_publisher(desired_pose)
 
         rospy.loginfo(
             "GPS Fix is Valid! Setting Navigation Goal to: %f, %f, %f",
@@ -140,13 +144,39 @@ class WaypointServer(object):
             desired_pose.pose.position.z)
 
         rospy.loginfo("Robot is heading %f metres at a bearing of %f degrees",
-                      self.disp_to_wp, (bearing_to_wp * 180 / pi + 360) % 360)
+                      sqrt(pow(desired_pose.pose.position.x,2)
+                      + pow(desired_pose.pose.position.y,2)),
+                      (bearing_to_wp * 180 / pi + 360) % 360)
+
+    def marker_publisher(self,desired_pose):
+        wp_marker = Marker()
+        wp_marker.header.frame_id = "map"
+        wp_marker.header.stamp = rospy.Time.now()
+        wp_marker.ns = "bezier"
+        wp_marker.action = wp_marker.ADD
+        wp_marker.type = wp_marker.SPHERE
+        wp_marker.id = self.wp_num
+
+        wp_marker.pose.position.x = desired_pose.pose.position.x
+        wp_marker.pose.position.y = desired_pose.pose.position.y
+        wp_marker.pose.position.z = desired_pose.pose.position.z
+
+        wp_marker.scale.x = 1
+        wp_marker.scale.y = 1
+        wp_marker.scale.z = 0.1
+
+        wp_marker.color.r = 1.0
+        wp_marker.color.g = 0
+        wp_marker.color.b = 0
+        wp_marker.color.a = 1.0
+
+        self.marker_pub.publish(wp_marker)
 
     def gps_subscriber(self, gps_msg):
         if self.generate_wp:
             GMapsWaypoint().get_gmaps_waypoint(gps_msg)
             self.generate_wp = False
-        elif gps_msg.status.status > -1 and not self.gps_fix:  # and self.reset_all:
+        elif gps_msg.status.status > -1 and not self.gps_fix:
             self.last_valid_fix_time = rospy.get_time()
             self.lat_curr = gps_msg.latitude
             self.lon_curr = gps_msg.longitude
@@ -163,10 +193,6 @@ class WaypointServer(object):
                     self.lat_origin, self.lon_origin)
 
     def robot_pose_subscriber(self, pose_msg):
-        # if self.reset_all:
-        #     self.origin_pos_x = pose_msg.pose.pose.position.x
-        #     self.origin_pos_y = pose_msg.pose.pose.position.y
-        #     self.origin_pos_z = pose_msg.pose.pose.position.z
         self.curr_pos_x = pose_msg.pose.pose.position.x
         self.curr_pos_y = pose_msg.pose.pose.position.y
         self.curr_pos_z = pose_msg.pose.pose.position.z
@@ -188,23 +214,21 @@ class WaypointServer(object):
     def fetch_next_waypoint(self):
         file = self.pkg + '/waypoint_viewer/coordinates.json'
         if os.path.exists(file):
-            # self.dist_from_origin = haversine_distance(
-            #     self.lat_origin, self.lon_origin, self.lat_wp, self.lon_wp)
-            # print(self.lat_origin,self.lon_origin,self.lat_wp,self.lon_wp,self.dist_from_origin,self.disp_to_wp)
-            if self.disp_to_wp <= self.threshold_distance or not self.wp_update_state:
-                with open(file) as f:
-                    coordinates = json.load(f)
-                self.wp_update_state = True
-                rospy.loginfo("Fetching Next WayPoint: %f,%f",
-                              coordinates[self.wp_num][0],
-                              coordinates[self.wp_num][1])
+            with open(file) as f:
+                coordinates = json.load(f)
+            self.num_of_waypoints = len(coordinates)
+            self.wp_update_state = True
+            rospy.loginfo("Fetching Next WayPoint: %f,%f",
+                          coordinates[self.wp_num][0],
+                          coordinates[self.wp_num][1])
 
-                self.lat_wp = coordinates[self.wp_num][0]
-                self.lon_wp = coordinates[self.wp_num][1]
-                self.wp_num += 1
-                self.new_waypoint = True
-                self.dist_from_origin = haversine_distance(
-                    self.lat_origin, self.lon_origin, self.lat_wp, self.lon_wp)
+            self.lat_wp = coordinates[self.wp_num][0]
+            self.lon_wp = coordinates[self.wp_num][1]
+            self.wp_num += 1
+            self.new_waypoint = True
+            self.dist_from_origin = haversine_distance(
+                self.lat_origin, self.lon_origin, self.lat_wp, self.lon_wp)
+            f.close()
         else:
             rospy.loginfo("Waypoints are not generated")
 
