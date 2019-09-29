383e8a023b0c2ce6247362edfa74a7e2a1dbe4e5
Sarathkrishnan Ramesh <sarathkrishnan99@gmail.com>
2019-09-29 20:25:27 +0530

    Generate intermediate waypoints using Google Maps

--- scripts/gmaps_waypoint.py
+++ scripts/gmaps_waypoint.py
@@ -9,12 +9,15 @@ import threading
 from datetime import datetime
 import googlemaps
 import rospkg
+from math import radians, degrees, asin, sin, cos, sqrt, atan2, pi, sqrt, pow, modf
+from geopy import distance
 
 
 class GMapsWaypoint(object):
     def __init__(self):
         self.pkg = rospkg.RosPack().get_path('waypoint_server')
-        print(self.pkg)
+        self.interval = 10.0
+        self.intermediate_waypoints = []
 
     def get_api_key(self):
         file_name = self.pkg + "/waypoint_viewer/api_key.txt"
@@ -22,13 +25,55 @@ class GMapsWaypoint(object):
             apikey = infile.readline()
             infile.close()
         return apikey
+    
+    def bearing(self, wp_source, wp_destination):
+        lat_wp, lon_wp, lat_origin, lon_origin = map(
+            radians, [wp_destination[0], wp_destination[1], wp_source[0], wp_source[1]])
+        d_lon = lon_wp - lon_origin
+        return atan2(
+            sin(d_lon) * cos(lat_wp),
+            ((cos(lat_origin) * sin(lat_wp)) -
+             (sin(lat_origin) * cos(lat_wp) * cos(d_lon))))
 
-    def get_gmaps_waypoint(self, data):
-        print("latitude: ", data.latitude)
-        print("longitude: ", data.longitude)
+    def get_destination_coordinates(self, wp_source, wp_bearing, wp_dist):
+        lat_source, lon_source = map(radians, [wp_source[0], wp_source[1]])
 
-        origin = data.latitude, data.longitude
-        dest = raw_input("Enter Destination: ")
+        delta = wp_dist / (distance.EARTH_RADIUS * 1000)
+        lat_wp = asin(
+            sin(lat_source) * cos(delta) +
+            cos(lat_source) * sin(delta) *
+            cos(wp_bearing))
+
+        lon_wp = lon_source + atan2(
+            sin(wp_bearing) * sin(delta) * cos(lat_source),
+            cos(delta) - sin(lat_source) * sin(lat_wp))
+
+        lat_wp, lon_wp = map(degrees, [lat_wp, lon_wp])
+
+        return (lat_wp, lon_wp)
+
+    def get_intermerdiate_waypoints(self, wp_source, wp_destination):
+        bearing_to_destination = self.bearing(wp_source, wp_destination)
+        distance_to_destination = distance.distance(wp_source, wp_destination).m
+        remaining, dist = modf((distance_to_destination / self.interval))
+
+        self.intermediate_waypoints.append(wp_source)
+
+        coordinates = wp_source
+        next_distance = self.interval
+
+        for i in range(int(dist)):
+            coordinates = self.get_destination_coordinates(wp_source, bearing_to_destination, next_distance)
+            next_distance += self.interval
+            self.intermediate_waypoints.append(coordinates)
+        
+        self.intermediate_waypoints.append(wp_destination)
+
+
+    def get_gmaps_waypoint(self):
+        origin = input("Enter Source: ")
+        dest = input("Enter Destination: ")
+        self.interval = int(input("Enter waypoint interval (meters): "))
 
         gmaps = googlemaps.Client(key=self.get_api_key())
 
@@ -36,6 +81,11 @@ class GMapsWaypoint(object):
         directions = gmaps.directions(origin, dest, mode="driving", departure_time=now)[0]
 
         coordinates = self.decode_polyline(directions['overview_polyline']['points'])
+        
+        for i in range(len(coordinates)-1):
+            self.get_intermerdiate_waypoints(coordinates[i], coordinates[i+1])
+
+        print(len(coordinates), len(self.intermediate_waypoints))
 
         file_name = self.pkg + "/waypoint_viewer/coordinates.json"
         with open(file_name, 'wb') as outfile:
@@ -43,9 +93,15 @@ class GMapsWaypoint(object):
             outfile.write(data.encode('UTF-8'))
             outfile.flush()
 
-        print("Opening Waypoint Viewer...")
-        t = threading.Thread(target=self.open_waypoint_viewer)
-        t.start()
+        file_name = self.pkg + "/waypoint_viewer/intermediate_coordinates.json"
+        with open(file_name, 'wb') as outfile:
+            data = json.dumps(self.intermediate_waypoints, indent=4, sort_keys=False)
+            outfile.write(data.encode('UTF-8'))
+            outfile.flush()
+
+        # print("Opening Waypoint Viewer...")
+        # t = threading.Thread(target=self.open_waypoint_viewer)
+        # t.start()
 
     def open_waypoint_viewer(self):
         os.system("firefox " + self.pkg + "/waypoint_viewer/home.html")
@@ -79,3 +135,11 @@ class GMapsWaypoint(object):
             coordinates.append((lat / 100000.0, lng / 100000.0))
 
         return coordinates
+
+
+def main():
+    g = GMapsWaypoint()
+    g.get_gmaps_waypoint()
+
+if __name__ == "__main__":
+    main()
--- waypoint_viewer/javascript/map.js
+++ waypoint_viewer/javascript/map.js
@@ -3,87 +3,87 @@ function initMap()
   var loc = {lat:13.34555, lng:74.77146};
   var map = new google.maps.Map(document.getElementById('map'), {
           center: loc,
-          zoom: 14,
-          styles: [
-            {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
-            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
-            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
-            {
-              featureType: 'administrative.locality',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
-            },
-            {
-              featureType: 'poi',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
-            },
-            {
-              featureType: 'poi.park',
-              elementType: 'geometry',
-              stylers: [{color: '#263c3f'}]
-            },
-            {
-              featureType: 'poi.park',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#6b9a76'}]
-            },
-            {
-              featureType: 'road',
-              elementType: 'geometry',
-              stylers: [{color: '#38414e'}]
-            },
-            {
-              featureType: 'road',
-              elementType: 'geometry.stroke',
-              stylers: [{color: '#212a37'}]
-            },
-            {
-              featureType: 'road',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#9ca5b3'}]
-            },
-            {
-              featureType: 'road.highway',
-              elementType: 'geometry',
-              stylers: [{color: '#746855'}]
-            },
-            {
-              featureType: 'road.highway',
-              elementType: 'geometry.stroke',
-              stylers: [{color: '#1f2835'}]
-            },
-            {
-              featureType: 'road.highway',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#f3d19c'}]
-            },
-            {
-              featureType: 'transit',
-              elementType: 'geometry',
-              stylers: [{color: '#2f3948'}]
-            },
-            {
-              featureType: 'transit.station',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#d59563'}]
-            },
-            {
-              featureType: 'water',
-              elementType: 'geometry',
-              stylers: [{color: '#17263c'}]
-            },
-            {
-              featureType: 'water',
-              elementType: 'labels.text.fill',
-              stylers: [{color: '#515c6d'}]
-            },
-            {
-              featureType: 'water',
-              elementType: 'labels.text.stroke',
-              stylers: [{color: '#17263c'}]
-            }
-          ]
+          zoom: 14//,
+          // styles: [
+          //   {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+          //   {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+          //   {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+          //   {
+          //     featureType: 'administrative.locality',
+          //     elementType: 'labels.text.fill',
+          //     stylers: [{color: '#d59563'}]
+          //   },
+          //   {
+          //     featureType: 'poi',
+          //     elementType: 'labels.text.fill',
+          //     stylers: [{color: '#d59563'}]
+          //   },
+          //   {
+          //     featureType: 'poi.park',
+          //     elementType: 'geometry',
+          //     stylers: [{color: '#263c3f'}]
+          //   },
+          //   {
+          //     featureType: 'poi.park',
+          //     elementType: 'labels.text.fill',
+          //     stylers: [{color: '#6b9a76'}]
+          //   },
+          //   {
+          //     featureType: 'road',
+          //     elementType: 'geometry',
+          //     stylers: [{color: '#38414e'}]
+          //   },
+          //   {
+          //     featureType: 'road',
+          //     elementType: 'geometry.stroke',
+          //     stylers: [{color: '#212a37'}]
+          //   },
+          //   {
+          //     featureType: 'road',
+          //     elementType: 'labels.text.fill',
+          //     stylers: [{color: '#9ca5b3'}]
+          //   },
+          //   {
+          //     featureType: 'road.highway',
+          //     elementType: 'geometry',
+          //     stylers: [{color: '#746855'}]
+          //   },
+          //   {
+          //     featureType: 'road.highway',
+          //     elementType: 'geometry.stroke',
+          //     stylers: [{color: '#1f2835'}]
+          //   },
+          //   {
+          //     featureType: 'road.highway',
+          //     elementType: 'labels.text.fill',
+          //     stylers: [{color: '#f3d19c'}]
+          //   },
+          //   {
+          //     featureType: 'transit',
+          //     elementType: 'geometry',
+          //     stylers: [{color: '#2f3948'}]
+          //   },
+          //   {
+          //     featureType: 'transit.station',
+          //     elementType: 'labels.text.fill',
+          //     stylers: [{color: '#d59563'}]
+          //   },
+          //   {
+          //     featureType: 'water',
+          //     elementType: 'geometry',
+          //     stylers: [{color: '#17263c'}]
+          //   },
+          //   {
+          //     featureType: 'water',
+          //     elementType: 'labels.text.fill',
+          //     stylers: [{color: '#515c6d'}]
+          //   },
+          //   {
+          //     featureType: 'water',
+          //     elementType: 'labels.text.stroke',
+          //     stylers: [{color: '#17263c'}]
+          //   }
+          // ]
         });
 
   var locations;
@@ -144,7 +144,8 @@ function initMap()
     botArea.setCenter(newLatLng);
   });
 
-  $.getJSON("coordinates.json", function(json) {
+  
+  $.getJSON("./coordinates.json", function(json) {
     locations = json;
 
     var len = Object.keys(locations).length;
@@ -172,5 +173,23 @@ function initMap()
       bounds.extend(marker.position);
     }
     map.fitBounds(bounds);
+  });
+
+  $.getJSON("./intermediate_coordinates.json", function(json) {
+    locations = json;
+
+    var len = Object.keys(locations).length;
+
+    for (i = 0; i < len; i++) {
+        marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][0], locations[i][1]),
+        map: map,
+        icon: {
+          path: google.maps.SymbolPath.CIRCLE,
+          scale: 2,
+          strokeColor: '#0000ff'
+        }
+      });
+    }
   });
 }
